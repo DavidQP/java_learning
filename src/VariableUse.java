36623bec9af4a56c8a9b5b2330d12f201700b1da
david <juda9323@gmail.com>
2025-04-05 17:46:28 -0500

    Add variableUse class to project

---
+++ src/VariableUse.java
@@ -0,0 +1,16 @@
+
+public class VariableUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte age = 17; // declare and initialize a variable
+		var name = "Hi, my name is ..."; // inference
+		var count = 0;
+		count = 99999999;
+		System.out.println(age);
+		System.out.println(count);
+		System.out.println(name);
+
+	}
+
+}
