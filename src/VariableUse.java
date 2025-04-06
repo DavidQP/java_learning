7be2ab7ba1592868220140224de70b476be665b6
david <juda9323@gmail.com>
2025-04-06 08:06:46 -0500

    Add multiples declaration and assignment examples

--- src/VariableUse.java
+++ src/VariableUse.java
@@ -10,6 +10,23 @@ public class VariableUse {
 		System.out.println(age);
 		System.out.println(count);
 		System.out.println(name);
+		System.out.println("--------------------");
+		System.out.println();
+		// Multiple declaration
+		int bill, tax, total;
+		bill = 56;
+		tax = 2;
+		total = bill + tax;
+		System.out.println("Bill   " + bill);
+		System.out.println("Tax    " + tax);
+		System.out.println("Total: " + total);
+		// Another example
+		System.out.println();
+		int apples = 5, bananas = 9, totalFruit = apples + bananas;
+		System.out.println("Total fruit: " + totalFruit);
+		// Assign one variable
+		int cundinamarcaPeople, boyacaPeople = 90000; // This line only assign a value to boyacaPeople variable
+		System.out.println("The people in Boyaca are: " + boyacaPeople);
 
 	}
 
